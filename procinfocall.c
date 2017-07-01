576926b80d710c77d892fa34498cb1007988b292
terzothethird <s.l.chiavegatto@gmail.com>
2017-07-01 20:40:10 -0300

    Update procinfocall.c

--- procinfocall.c
+++ procinfocall.c
@@ -7,6 +7,8 @@
 
 asmlinkage long sys_listProcessInfo(void) {
     struct task_struct *proc;
+    struct sysinfo si;
+    sysinfo (&si);
  
     for_each_process(proc) {
        
@@ -22,6 +24,8 @@ asmlinkage long sys_listProcessInfo(void) {
            proc->comm, (long)task_pid_nr(proc), actual_time_sec
        );
   
+       printk("Tempo do sistema: %ld dias, %ld:%02ld:%02ld\n", si.uptime/(24*60*60), (si.uptime % (24*60*60))/60*60,
+             (si.uptime % (60*60))/60, si.uptime % 60));
        printk("\n\n");
 
    }
